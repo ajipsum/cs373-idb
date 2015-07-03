2b65aced3dfea0a8b0aac115d7b2319c4c174aae
John Peden <john.peden@utexas.edu>
2015-07-03 14:28:27 -0500

    resolved #36

--- __init__.py
+++ __init__.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/table")
+def table():
+    return render_template('table.html')
+
 if __name__ == "__main__":
-  #  app.debug = True
-    app.run(host='0.0.0.0', port=80)
+    app.debug = True
+    app.run(host='127.0.0.1', port=80)
--- static/assets/javascript/app.js
+++ static/assets/javascript/app.js
@@ -1,4 +1,7 @@
-var app = angular.module('api2k15', []);
+var app = angular.module('api2k15', [
+    'ngTouch',
+    'ui.grid'    
+]);
 
 app.config(function($interpolateProvider) {
     $interpolateProvider.startSymbol('{[');
