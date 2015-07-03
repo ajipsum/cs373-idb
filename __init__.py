390197c6917c5dea2b6f2b67fca25878190d3934
tiras-j <joshua.tiras@utexas.edu>
2015-07-03 15:40:57 -0500

    Updated css issue causing problems with Chrome. Closes #45

--- __init__.py
+++ __init__.py
@@ -12,15 +12,11 @@ def index(static = None):
             abort(404)
     else :
         return render_template('index.html')
-"""
-@app.route("/test1")
-def test1():
-    return render_template('test1.html')
-"""
+
 @app.route("/table")
 def table():
     return render_template('table.html')
 
 if __name__ == "__main__":
     app.debug = True
-    app.run(host='127.0.0.1', port=80)
+    app.run()
