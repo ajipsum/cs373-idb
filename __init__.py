d84d1b59b466db2aaf347ceeb4e3a862f7a32f70
okruhlik <ezokruhlik@gmail.com>
2015-07-05 13:07:27 -0500

    Set __init__.py to run locally

--- __init__.py
+++ __init__.py
@@ -1,12 +1,22 @@
-#!/usr/bin/env python3
-
-from flask import Flask, send_file, render_template
+from flask import Flask, send_file, render_template, abort
+from jinja2 import TemplateNotFound
 app = Flask(__name__)
 
 @app.route("/")
-def index():
-    return render_template('index.html')
+@app.route("/<static>")
+def index(static = None):
+    if static : 
+        try:
+            return render_template(static + '.html')
+        except:
+            abort(404)
+    else :
+        return render_template('index.html')
+
+@app.route("/table")
+def table():
+    return render_template('table.html')
 
 if __name__ == "__main__":
-  #  app.debug = True
-    app.run(host='0.0.0.0', port=80)
+    app.debug = True
+    app.run()
