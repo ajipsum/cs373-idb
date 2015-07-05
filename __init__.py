3477f19c9d3a0b28121eee9516ed1a872b084d41
lazercorn@github.com/cs378-summer-2015/collatz-tests <perry_feng@utexas.edu>
2015-07-05 03:46:49 -0500

    indentation and missing primary key

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 from flask import Flask, send_file, render_template
 from flask.ext.sqlalchemy import SQLAlchemy
+import models
+
 
 app = Flask(__name__)
 app.config.from_object('config')
--- models.py
+++ models.py
@@ -2,69 +2,76 @@ from __init__ import db
 
 
 class Player(db.Model):
-    '''
-    Information about player
-    Information includes name, picture, position, player number, weight etc.
-    '''
-    name = db.Column(db.String(256), primary_key=True)
-    picture = db.Column(db.String(256), unique=True)
-    experience_years = db.Column(db.String(256))
-    draft_info = db.Column(db.String(256))
-    position = db.Column(db.String(256))
-    player_number = db.Column(db.String(256))
-    current_team = db.Column(db.String(256))
-    college = db.Column(db.String(256))
-    birth_info = db.Column(db.String(256))
-    weight = db.Column(db.String(256))
-    twitter = db.Column(db.String(256))
-    age = db.Column(db.String(256))
-    youtube_link_1 = db.Column(db.String(256))
-    youtube_link_2 = db.Column(db.String(256))
-    youtube_link_3 = db.Column(db.String(256))
-    career_GS = db.Column(db.String(256))
-    career_REB = db.Column(db.String(256))
-    career_BLK = db.Column(db.String(256))
-    career_FT_PCT = db.Column(db.String(256))
-    career_DR = db.Column(db.String(256))
-    career_MIN = db.Column(db.String(256))
-    career_FG_PCT = db.Column(db.String(256))
-    career_3PM_A = db.Column(db.String(256))
-    career_OR = db.Column(db.String(256))
-    career_FGM_A = db.Column(db.String(256))
-    career_STL = db.Column(db.String(256))
-    career_TO = db.Column(db.String(256))
-    career_3PM_PCT = db.Column(db.String(256))
-    career_AST = db.Column(db.String(256))
-    career_GP = db.Column(db.String(256))
-    career_PF = db.Column(db.String(256))
-    career_PTS = db.Column(db.String(256))
-    CAREER_FTM_A = db.Column(db.String(256))
-    season_TO = db.Column(db.String(256))
-    season_GS = db.Column(db.String(256))
-    season_FG_PCT = db.Column(db.String(256))
-    seasonseason__PTS = db.Column(db.String(256))
-    season_OR = db.Column(db.String(256))
-    season_GP = db.Column(db.String(256))
-    season_PF = db.Column(db.String(256))
-    season_REB = db.Column(db.String(256))
-    season_FTM_A = db.Column(db.String(256))
-    season_BLK = db.Column(db.String(256))
-    season_MIN = db.Column(db.String(256))
-    season_STL = db.Column(db.String(256))
-    season_AST = db.Column(db.String(256))
-    season_FT_PCT = db.Column(db.String(256))
-    season_FGM_A = db.Column(db.String(256))
-    season_3P_PCT = db.Column(db.String(256))
-    season_DR = db.Column(db.String(256))
-    season_3PM_A = db.Column(db.String(256))
-    team_name = db.Column(db.String(256), db.ForeignKey('team.name'))
+  '''
+  Information about player
+  Information includes name, picture, position, player number, weight etc.
+  '''
 
-    # def __init__(self, username, email):
-    #     self.username = username
-    #     self.email = email
+  __tablename__ = "Player"
 
-    # def __repr__(self):
-    #     return '<User %r>' % self.username
+  name = db.Column(db.String(256), primary_key=True)
+  picture = db.Column(db.String(256), unique=True)
+  experience_years = db.Column(db.String(256))
+  draft_info = db.Column(db.String(256))
+  position = db.Column(db.String(256))
+  player_number = db.Column(db.String(256))
+  current_team = db.Column(db.String(256))
+  college = db.Column(db.String(256))
+  birth_info = db.Column(db.String(256))
+  weight = db.Column(db.String(256))
+  twitter = db.Column(db.String(256))
+  age = db.Column(db.String(256))
+  youtube_link_1 = db.Column(db.String(256))
+  youtube_link_2 = db.Column(db.String(256))
+  youtube_link_3 = db.Column(db.String(256))
+  career_GS = db.Column(db.String(256))
+  career_REB = db.Column(db.String(256))
+  career_BLK = db.Column(db.String(256))
+  career_FT_PCT = db.Column(db.String(256))
+  career_DR = db.Column(db.String(256))
+  career_MIN = db.Column(db.String(256))
+  career_FG_PCT = db.Column(db.String(256))
+  career_3PM_A = db.Column(db.String(256))
+  career_OR = db.Column(db.String(256))
+  career_FGM_A = db.Column(db.String(256))
+  career_STL = db.Column(db.String(256))
+  career_TO = db.Column(db.String(256))
+  career_3PM_PCT = db.Column(db.String(256))
+  career_AST = db.Column(db.String(256))
+  career_GP = db.Column(db.String(256))
+  career_PF = db.Column(db.String(256))
+  career_PTS = db.Column(db.String(256))
+  CAREER_FTM_A = db.Column(db.String(256))
+  season_TO = db.Column(db.String(256))
+  season_GS = db.Column(db.String(256))
+  season_FG_PCT = db.Column(db.String(256))
+  seasonseason__PTS = db.Column(db.String(256))
+  season_OR = db.Column(db.String(256))
+  season_GP = db.Column(db.String(256))
+  season_PF = db.Column(db.String(256))
+  season_REB = db.Column(db.String(256))
+  season_FTM_A = db.Column(db.String(256))
+  season_BLK = db.Column(db.String(256))
+  season_MIN = db.Column(db.String(256))
+  season_STL = db.Column(db.String(256))
+  season_AST = db.Column(db.String(256))
+  season_FT_PCT = db.Column(db.String(256))
+  season_FGM_A = db.Column(db.String(256))
+  season_3P_PCT = db.Column(db.String(256))
+  season_DR = db.Column(db.String(256))
+  season_3PM_A = db.Column(db.String(256))
+  team_name = db.Column(db.String(256), db.ForeignKey('team.name'))
+
+  # def __init__(self, name, picture):
+  #   self.name = name
+  #   self.picture = picture
+
+  # def __init__(self, username, email):
+  #     self.username = username
+  #     self.email = email
+
+  # def __repr__(self):
+  #     return '<User %r>' % self.username
 
 
 class Team(db.Model):
@@ -72,8 +79,11 @@ class Team(db.Model):
   Information about Team 
   Information includes name, conference, division, site_name, city, state, mascot
   '''
+
+  __tablename__ = "Team"
+
   players = db. relationship('Player', backref='team', lazy='dynamic')
-  name = db.Column(db.String(256))
+  name = db.Column(db.String(256), primary_key=True)
   conference = db.Column(db.String(256))
   division = db.Column(db.String(256))
   site_name = db.Column(db.String(256))
@@ -82,13 +92,13 @@ class Team(db.Model):
   mascot = db.Column(db.String(256))
 
 teams = db.Table('teams',
-    db.Column('team_name', db.String(256), db.ForeignKey('team.name')),
-    db.Column('game_id', db.Integer, db.ForeignKey('game.id'))
+  db.Column('team_name', db.String(256), db.ForeignKey('team.name')),
+  db.Column('game_id', db.Integer, db.ForeignKey('game.id'))
 )
 
 players = db.Table('players',
-    db.Column('player_name', db.String(256), db.ForeignKey('player.name')),
-    db.Column('game_id', db.Integer, db.ForeignKey('game.id'))
+  db.Column('player_name', db.String(256), db.ForeignKey('player.name')),
+  db.Column('game_id', db.Integer, db.ForeignKey('game.id'))
 )
 
 class Game(db.Model):
@@ -96,6 +106,9 @@ class Game(db.Model):
   Information about Game
   Information include home_team, away_team, data, home_score, away_score, etc.
   '''
+
+  __tablename__ = "Game"
+
   id = db.Column(db.Integer, primary_key=True)
   home_team = db.Column(db.String(256))
   away_team = db.Column(db.String(256))
@@ -135,11 +148,11 @@ class Game(db.Model):
 
   #many to many team game relationship
   teams = db.relationship('Team', secondary=teams,
-      backref=db.backref('games', lazy='dynamic'))
+    backref=db.backref('games', lazy='dynamic'))
 
   #many to many player game relationship
   players = db.relationship('Player', secondary=players,
-      backref=db.backref('games', lazy='dynamic'))
+    backref=db.backref('games', lazy='dynamic'))
 
 
 
