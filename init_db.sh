503230852748544fc9695837a2129063265784c9
tiras-j <joshua.tiras@utexas.edu>
2015-07-07 20:41:07 -0500

    Changed bash script to execute mysql without REPL

--- init_db.sh
+++ init_db.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-mysql -u root
-CREATE USER 'api2k15'@'localhost';
-GRANT ALL PRIVILEGES ON *.* TO 'api2k15'@'localhost';
-quit;
-mysql -u api2k15
-create database nba_flask_test;
+mysql --user="root" --execute="CREATE USER 'api2k15'@'localhost'; GRANT ALL PRIVILEGES ON *.* TO 'api2k15'@'localhost';"
+mysql --user="api2k15" --execute="CREATE DATABASE nba_flask_test;"
+
 
