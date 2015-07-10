1168bf23fb8c4523ddadb49a3ccea2fe2be0e7c5
John Peden <john.peden@utexas.edu>
2015-07-10 16:00:28 -0500

    finished bracketization for game and player templates #82

--- assets/javascript/controllers/GameDetailCtrl.js
+++ assets/javascript/controllers/GameDetailCtrl.js
@@ -1,5 +1,17 @@
 app.controller('GameDetailCtrl', ['$scope', function ($scope) {
 
-	$scope.gameID='gameId_from_json';
+	$scope.gameDate='gameDate_from_json';
+	$scope.awayId='awayTeamId_from_json';
+	$scope.awayLogo='awayLogo_from_json';
+	$scope.homeId='homeTeamId_from_json';
+	$scope.homeLogo='homeLogo_from_json';
+	$scope.awayName='awayName_from_json';
+	$scope.awayScore='awayScore_from_json';
+	$scope.homeName='homeName_from_json';
+	$scope.homeScore='homeScore_from_json';
+	$scope.awayPlayerObj='awayPlayerObj_from_json';
+	$scope.homePlayerObj='homePlayerObj_from_json';
+	$scope.gameMapSource='gameMapSource_from_json';
+	$scope.gameCitation='gameCitation_from_json';
 
 }]);
