32e61f771e7dc9b471e519ed5a08e217b7fab08d
John Peden <john.peden@utexas.edu>
2015-07-11 16:29:03 -0500

    adding dynamic serving

--- assets/javascript/controllers/PlayerDetailCtrl.js
+++ assets/javascript/controllers/PlayerDetailCtrl.js
@@ -1,18 +1,17 @@
 app.controller('PlayerDetailCtrl', ['$scope', 'player', function ($scope, player) {
     
-    console.log(player);
 	$scope.playerHeadshot = player.picture;
 	$scope.playerName= player.name;
-	$scope.playerNumber='playerNumber_from_json';
-	$scope.playerPosition='playerPosition_from_json';
-	$scope.teamId='teamId_from_json';
-	$scope.teamName='teamName_from_json';
-	$scope.twitterAccount='twitterAccount_from_json';
-	$scope.age='age_from_json';
-	$scope.weight='weight_from_json';
+	$scope.playerNumber=player.playerNumber;
+	$scope.playerPosition=player.position;
+	$scope.teamId=player.team_name;
+	$scope.teamName=player.current_team;
+	$scope.twitterAccount=player.twitter;
+	$scope.age=player.age;
+	$scope.weight=player.weight;
 	$scope.gameObj='gameObj_from_json';
-	$scope.playerVideo='playerVideo_from_json';
+	$scope.playerVideo=player.youtube_link_1;
 	$scope.playerMapSource='playerMapSource_from_json';
-	$scope.playerCitation='playerCitation_from_json';
+	$scope.playerCitation=player.citation;
 
 }]);
