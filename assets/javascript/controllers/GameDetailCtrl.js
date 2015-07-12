66cfba33166d57e3dbfbfe47fc558b5bb6ad2b05
John Peden <john.peden@utexas.edu>
2015-07-12 18:26:04 -0500

    table populate and link now, plus some individual page work

--- assets/javascript/controllers/GameDetailCtrl.js
+++ assets/javascript/controllers/GameDetailCtrl.js
@@ -1,17 +1,17 @@
 app.controller('GameDetailCtrl', ['$scope', function ($scope, game) {
 
-	$scope.gameDate='gameDate_from_json';
-	$scope.awayId='awayTeamId_from_json';
-	$scope.awayLogo='awayLogo_from_json';
-	$scope.homeId='homeTeamId_from_json';
-	$scope.homeLogo='homeLogo_from_json';
-	$scope.awayName='awayName_from_json';
-	$scope.awayScore='awayScore_from_json';
-	$scope.homeName='homeName_from_json';
-	$scope.homeScore='homeScore_from_json';
-	$scope.awayPlayerObj='awayPlayerObj_from_json';
-	$scope.homePlayerObj='homePlayerObj_from_json';
-	$scope.gameMapSource='gameMapSource_from_json';
-	$scope.gameCitation='gameCitation_from_json';
+	$scope.gameDate=game.date;
+	$scope.awayId='awayTeamId_from_json'; //not implemented, need id from team (maybe foreign key)
+	$scope.awayLogo='awayLogo_from_json'; //not implemented, team (or game) needs to store path to team logo
+	$scope.homeId='homeTeamId_from_json'; //not implemented, need id from team (maybe foreign key)
+	$scope.homeLogo='homeLogo_from_json'; //not implemented, team (or game) needs to store path to team logo
+	$scope.awayName=game.away_team;
+	$scope.awayScore=game.away_score;
+	$scope.homeName=game.home_team;
+	$scope.homeScore=game.home_score;
+	$scope.awayPlayerObj=game.away_roster; //not implemented, need the roster from team
+	$scope.homePlayerObj=game.home_roster; //not implemented, need the roster from team
+	$scope.gameMapSource=game.google_maps;
+	$scope.gameCitation=game.citation;
 
 }]);
--- assets/javascript/controllers/TeamsCtrl.js
+++ assets/javascript/controllers/TeamsCtrl.js
@@ -1,49 +1,15 @@
 app.controller('TeamsCtrl', function($scope, teams) {
-    $scope.teams = teams;    
-
-    $scope.teamData = [
-        {
-            "last_name": "Spurs",
-            "city": "San Antonio",
-            "state": "Texas",
-            "site_name": "AT&T Center",
-            "conference": "West",
-            "division": "Southwest",
-            "mascot": "The Coyote",
-            "tag": "sas"
-        },
-        {
-            "last_name": "Heat",
-            "city": "Miami",
-            "state": "Florida",
-            "site_name": "AmericanAirlines Arena",
-            "conference": "East",
-            "division": "Southeast",
-            "mascot": "Burnie",
-            "tag": "mia"
-        },
-        {
-            "last_name": "Clippers",
-            "city": "Los Angeles",
-            "state": "California",
-            "site_name": "Staples Center",
-            "conference": "West",
-            "division": "Pacific",
-            "mascot": "Clippy",
-            "tag": "lac"
-        }
-    ];
 
     $scope.teamOptions = {
-        data: 'teams',
+        data: teams,
         paginationPageSizes: [5, 10, 15, 30],
         paginationPageSize: 15,
         columnDefs: 
         [
             {
-               field:'last_name', 
+               field:'name', 
                displayName:'Name',
-               cellTemplate: '<div class="ngCellText"><a href="/{{ row.entity.tag }}">{{ COL_FIELD }}</a></div>'
+               cellTemplate: '<div class="ngCellText"><a href="/{{ row.entity.name }}">{{ COL_FIELD }}</a></div>'
             },
             {
                field:'city', 
