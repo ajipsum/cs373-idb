3b931e21809ad1e55066e12506dcf57296dfd576
John Peden <john.peden@utexas.edu>
2015-07-12 19:26:22 -0500

    individual player page done

--- assets/javascript/controllers/PlayerDetailCtrl.js
+++ assets/javascript/controllers/PlayerDetailCtrl.js
@@ -1,5 +1,5 @@
-app.controller('PlayerDetailCtrl', ['$scope', 'player', function ($scope, player) {
-    
+app.controller('PlayerDetailCtrl', ['$scope', 'player', '$http', '$sce', function ($scope, player, $http, $sce) {
+
 	$scope.playerHeadshot = player.picture;
 	$scope.playerName= player.name;
 	$scope.playerNumber=player.playerNumber;
@@ -10,8 +10,10 @@ app.controller('PlayerDetailCtrl', ['$scope', 'player', function ($scope, player
 	$scope.age=player.age;
 	$scope.weight=player.weight;
 	$scope.gameObj=player.schedule; //schedule is a list of game objects
-	$scope.playerVideo=player.youtube_link_1;
-	$scope.playerMapSource='playerMapSource_from_json';
+	$scope.playerVideo=$sce.trustAsResourceUrl(player.youtube_link_1);
+	$http.get("/resources/team/" + player.team_name).success(function(response) {
+		$scope.playerMapSource = $sce.trustAsResourceUrl("https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!" + response.google_maps)
+	});
 	$scope.playerCitation=player.citation;
 
 }]);
