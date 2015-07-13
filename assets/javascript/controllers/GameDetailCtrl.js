79193f9d658830531496aa2b398cd6c4143de88f
John Peden <john.peden@utexas.edu>
2015-07-13 17:53:07 -0500

    work in progress

--- assets/javascript/controllers/GameDetailCtrl.js
+++ assets/javascript/controllers/GameDetailCtrl.js
@@ -9,8 +9,8 @@ app.controller('GameDetailCtrl', ['$scope', 'game', '$http', '$sce', function ($
 	$scope.awayScore=game.away_score;
 	$scope.homeName=game.home_team;
 	$scope.homeScore=game.home_score;
-	$scope.awayPlayerObj=game.away_roster; //not implemented, need the roster from team
-	$scope.homePlayerObj=game.home_roster; //not implemented, need the roster from team
+	$scope.awayRoster=game.roster.away;
+	$scope.homeRoster=game.roster.home;
 	$http.get("/resources/team/" + game.home_team).success(function(response) {
 		$scope.gameMapSource = $sce.trustAsResourceUrl("https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!" + response.google_maps)
 	});
