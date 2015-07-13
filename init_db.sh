7784519f8bebdebc1db1ceee6b263c98f6dfcacb
Brandon Gottesman <brandon.gottesman@gmail.com>
2015-07-13 21:49:39 +0000

    hopefully fixed travis ci issue

--- init_db.sh
+++ init_db.sh
@@ -4,6 +4,6 @@ export PATH=$PATH:/usr/local/mysql/bin
 mysql --user="root" --execute="CREATE USER 'api2k15'@'localhost'; GRANT ALL PRIVILEGES ON *.* TO 'api2k15'@'localhost';"
 mysql --user="api2k15" --execute="CREATE DATABASE nba_flask;"
 mysql --user="api2k15" --execute="CREATE DATABASE nba_flask_test;"
-mysql --user="root" --execute="SET @@global.innodb_large_prefix = 1;"
+mysql --user="api2k15" --execute="set GLOBAL innodb_large_prefix=ON;"
 
 
