575c9f520cc17bf0e39ee6a0bbc94ca2d27940f6
Brandon Gottesman <brandon.gottesman@gmail.com>
2015-07-15 00:29:54 +0000

    resolved  #127

--- __init__.py
+++ __init__.py
@@ -187,5 +187,5 @@ def games_by_site(site):
     return api_handlers.game_by_site_handler(site)
 
 if __name__ == "__main__":
-    #app.debug = True
+ #   app.debug = True
     app.run(host="0.0.0.0", port=80)
