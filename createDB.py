ea3859d37d7b1de238b0d07e482bd02c74904866
John Peden <john.peden@utexas.edu>
2015-07-21 15:19:54 -0500

    tweaking travis

--- createDB.py
+++ createDB.py
@@ -8,6 +8,6 @@ from db_workaround import db_tests
 import models, models_tests
 
 # create db schema based on model
-db.create_all()
+#db.create_all()
 db_tests.create_all()
 print('Database schema created\n')
